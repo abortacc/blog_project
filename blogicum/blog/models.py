ce8bef1f52b8df67fa5d5c94f4452451cf611035
Артём Тренин <gruzzli.hook@yandex.ru>
2025-01-25 14:54:26 +0300

    Create models and core folder

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from core.models import IsPubslishedClass
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Category(IsPubslishedClass):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+
+class Location(IsPubslishedClass):
+    name = models.CharField(max_length=256)
+
+
+class Post(IsPubslishedClass):
+    title = models.CharField(max_length=256)
+    text = models.TextField()
+    pub_date = models.DateTimeField()
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
